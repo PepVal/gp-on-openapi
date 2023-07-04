e222b21f4db94d92502b39637f032244ca72c088
PepVal <pepval001@gmail.com>
2023-07-04 16:02:04 -0500

    change path favicon

--- docs/make.sh
+++ docs/make.sh
@@ -7,5 +7,5 @@ cd ./docs
 # Bundle docs into zero-dependency HTML file
 npx @redocly/cli build-docs ./openapi.yaml --output ./public/index.html && \
 # Add favicon
-sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' index.html && \
+sed -i '7 i \ \ <link rel="icon" type="image/png" href="images/favicon.png"/>' ./public/index.html && \
 echo -e "\nDone!"
